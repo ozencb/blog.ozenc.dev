c5765a6b5c5610a6c5362143a360f85580dbc732
Özenç Bilgili <18405305+ozencb@users.noreply.github.com>
2025-09-14 01:24:08 +0300

    Update site title

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
-export const SITE_TITLE = 'Özenç Bilgili';
+export const SITE_TITLE = "özenç's blog";
 export const SITE_DESCRIPTION = "Özenç Bilgili's articles and projects";
 
-export const DATE_FORMAT = 'dd/MM/yyyy';
-export const DATE_FORMAT_SHORT = 'MMM dd yy';
+export const DATE_FORMAT = "dd/MM/yyyy";
+export const DATE_FORMAT_SHORT = "MMM dd yy";
 
 export const __DEV__ = import.meta.env.DEV;
 
