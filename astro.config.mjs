b6fb9108dfbd3cc7530d9bd349da53ca7f6185f0
Özenç Bilgili <18405305+ozencb@users.noreply.github.com>
2025-09-14 22:19:42 +0300

    Remove mermaid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig, sharpImageService } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import rehypeMermaid from "rehype-mermaid";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,11 +8,5 @@ export default defineConfig({
   output: "static",
   image: {
     service: sharpImageService(),
-  },
-  markdown: {
-    syntaxHighlight: {
-      excludeLangs: ["mermaid", "math"],
-    },
-    // rehypePlugins: [rehypeMermaid],
   },
 });
