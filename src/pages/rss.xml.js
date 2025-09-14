37203e1cb2b2c28726425166d76aa66756a26b95
Özenç Bilgili <18405305+ozencb@users.noreply.github.com>
2025-09-14 22:22:03 +0300

    Fix rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,12 @@
-import rss from '@astrojs/rss';
-import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
+import rss from "@astrojs/rss";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
 const parser = new MarkdownIt();
 
-import { SITE_TITLE, SITE_DESCRIPTION, __DEV__ } from '../constants';
-import { getAllArticles } from '../utils';
+import { SITE_TITLE, SITE_DESCRIPTION, __DEV__ } from "../constants";
+import { getAllArticles } from "../utils";
 
-export const get = async () => {
+export async function GET(context) {
   const posts = await getAllArticles();
 
   return rss({
@@ -14,11 +14,11 @@ export const get = async () => {
     description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
     items: posts.map((post) => ({
-      link: '/' + post.slug,
+      link: "/" + post.slug,
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      content: sanitizeHtml(parser.render(post.body))
-    }))
+      content: sanitizeHtml(parser.render(post.body)),
+    })),
   });
-};
+}
